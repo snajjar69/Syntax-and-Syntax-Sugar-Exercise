bb10cc821d088c353c62bfb6dcaf2fcdddafa934
snajjar69 <snajjar69@gmail.com>
2024-07-12 12:12:09 -0400

    added ternary operator

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -2,9 +2,24 @@
 {
     public class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //inferred Typing
+            var answer = 4;
+            string response;
+
+            Console.WriteLine(answer < 9 ? $"{answer} is less than nine" : $"{answer} is granter than or equal to nine");
+            
+                
+            // if (answer > 9)
+            // {
+            //    response = answer + "is less than Nine";
+            //  }
+            //  else
+            // {
+            //     Console.WriteLine(response = answer + "is greater than or equal to nine");
+            //  }
+
         }
     }
 }
